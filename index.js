148bf52a5d2ecca0dd3ef38c9ddae3e1f68b3836
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-07-26 19:42:57 +0900

    use modern ECMAScript features
    [BREAKING] drop support for Node.js < 4

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-/* eslint-disable no-var, prefer-template */
 'use strict';
 
-var karmaChromeLauncher = require('karma-chrome-launcher');
-
-[
-	'chrome',
-	'chromeCanary',
-	'chromium'
-].forEach(function setProp(propertyName) {
-	exports[propertyName] = karmaChromeLauncher[
-	'launcher:' +
-		propertyName.charAt(0).toUpperCase() +
-		propertyName.slice(1)
-	][1].prototype.DEFAULT_CMD[process.platform] || null;
-});
-
-/*
 const karmaChromeLauncher = require('karma-chrome-launcher');
 
 for (const propertyName of [
@@ -29,4 +13,3 @@ for (const propertyName of [
 		propertyName.slice(1)
 	}`][1].prototype.DEFAULT_CMD[process.platform] || null;
 }
-*/
