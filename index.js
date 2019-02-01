6196933c6b549717e70d8a4a0db599846fe5290f
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2019-02-01 17:49:56 +0900

    simplify property name computation

--- index.js
+++ index.js
@@ -2,14 +2,6 @@
 
 const karmaChromeLauncher = require('karma-chrome-launcher');
 
-for (const propertyName of [
-	'chrome',
-	'chromeCanary',
-	'chromium'
-]) {
-	exports[propertyName] = karmaChromeLauncher[`launcher:${
-		propertyName.charAt(0).toUpperCase()
-	}${
-		propertyName.slice(1)
-	}`][1].prototype.DEFAULT_CMD[process.platform] || null;
+for (const end of ['hrome', 'hromeCanary', 'hromium']) {
+	exports[`c${end}`] = karmaChromeLauncher[`launcher:C${end}`][1].prototype.DEFAULT_CMD[process.platform] || null;
 }
